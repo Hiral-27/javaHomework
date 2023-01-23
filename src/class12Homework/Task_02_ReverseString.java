c1fc0ac755f3a5bf409d2dbe14305bc77a88df55
Hiral-27 <hiral27496@gmail.com>
2023-01-23 14:45:33 +0530

    Java class-12 Homework

--- src/class12Homework/Task_02_ReverseString.java
+++ src/class12Homework/Task_02_ReverseString.java
@@ -6,11 +6,25 @@ public class Task_02_ReverseString {
         Task 2:
          Create a String and print it in reverse order (Sunday → yadnuS).
          */
+
+        // Method 1:
+        System.out.println("Method 1:");
         StringBuilder sb = new StringBuilder();
         String string= "Sunday";
         sb.append(string);
         sb.reverse();
         System.out.println(sb);
+
+        System.out.println("****************************");
+
+        // Method 2:
+        System.out.println("Method2:");
+        String day= "Sunday";
+        String reverse= "";
+        for (int i = 0; i < day.length(); i++) {
+            reverse= day.charAt(i)+reverse;
+        }
+        System.out.println(reverse);
 
     }
 }
