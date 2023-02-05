c36d30c27d08b1e002ca40d968f2ddcc7c382940
Hiral-27 <hiral27496@gmail.com>
2023-02-05 11:47:14 +0530

    Java Class 18 Homework

---
+++ src/class18Homework/A.java
@@ -0,0 +1,24 @@
+package class18Homework;
+
+public class A {
+    /*
+   Task 2:
+    Write program to inherit class A that has method printF which is static and call or reuse that method into class B
+     */
+
+    static void printF(){
+        System.out.println("Java");
+    }
+}
+
+class B extends A{
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        B b= new B();
+        B.printF();
+    }
+
+}
